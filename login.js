27afa990e17278956fa8795997d06599b2ab63dd
Micael Pereira <micael.pereira1999@hotmail.com>
2020-02-27 18:08:13 -0300

    decoupled user creation

--- login.js
+++ login.js
@@ -4,12 +4,31 @@ const mongoose = require('mongoose')
 const AccountModel = mongoose.model('Account')
 
 module.exports = () => {
-  router.post('signup', async (req, res) => {
+  router.post('signup', new SignUpRouter().route)
+}
+
+// Route
+class SignUpRouter {
+  async route (req, res) {
     const { email, password, repeatPassword } = req.body
-    if (password === repeatPassword) {
-      const user = await AccountModel.create({ email, password })
-      return res.json(user)
-    }
+    new SignUpUseCase().signUp(email, password, repeatPassword)
     res.status(400).json({ error: 'password must be equal to repeatPassword' })
-  })
+  }
+}
+
+// Use case
+class SignUpUseCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+// Database
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
+  }
 }
